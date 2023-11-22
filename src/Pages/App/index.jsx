674c87b8d525515ba95c0a826193c486ebcd9648
Tob Stoic <rafaeltobass@gmail.com>
2023-11-22 14:43:04 -0500

    Creado Navbar usando activeStyle. con udnerline y offset

--- src/Pages/App/index.jsx
+++ src/Pages/App/index.jsx
@@ -1,26 +1,31 @@
+import { BrowserRouter, useRoutes } from 'react-router-dom'
 import Home from '../Home'
 import MyAccount from '../MyAccount'
 import MyOrder from '../MyOrder'
 import MyOrders from '../MyOrders'
 import NotFound from '../NotFound'
 import SignIn from '../SignIn'
-
 import './App.css'
+import Navbar from '../../Components/Navbar'
 
-function App() {
+const AppRoutes = () => {
+  let routes = useRoutes([
+    { path: '/', element: <Home /> },
+    { path: '/my-account', element: <MyAccount /> },
+    { path: '/my-order', element: <MyOrder /> },
+    { path: '/my-orders', element: <MyOrders /> },
+    { path: '/*', element: <NotFound /> },
+    { path: '/sign-in', element: <SignIn /> }
+  
+  ])
+}
 
-
+const App = () => {
   return (
-    <div>
-      <Home />
-      <MyAccount />
-      <MyOrder />
-      <MyOrders />
-      <NotFound />
-      <SignIn />
-
-
-    </div>
+    <BrowserRouter>
+      <AppRoutes />
+      <Navbar />
+    </BrowserRouter>
   )
 }
 
