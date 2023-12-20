72e5fc9ccf5bb7a8c9c82f13de724d8817e9a189
Tob Stoic <rafaeltobass@gmail.com>
2023-12-20 05:26:46 -0500

    Mostrando producto en pproduct detail. accion de abrir y cerrar el aside de product detail

--- src/Components/ProductDetail/index.jsx
+++ src/Components/ProductDetail/index.jsx
@@ -1,12 +1,39 @@
 import { XMarkIcon } from '@heroicons/react/24/solid'
 import './style.css'
+import { useContext } from 'react'
+import { ShoppingCartContext } from '../../Context'
+
+
 const ProductDetail = () => {
+    const context = useContext(ShoppingCartContext)
+    console.log('product to show: ', context.productToShow)
     return (
-        <aside className='product-detail flex flex-col fixed bg-white right-0 border border-black rounded-lg'>
+        <aside 
+        className={`${context.isProductDetailOpen ? 'flex' : 'hidden'} product-detail flex-col fixed bg-white right-0 border border-black rounded-lg`}>
             <div className='flex justify-between items-center p-6'>
                 <h2 className='font-medium text-xl'>Detail</h2>
-                <div><XMarkIcon className='h-6 w-6 text-black-' /></div>
+                <div onClick={() => context.closeProductDetail()}>
+                    <XMarkIcon className='h-6 w-6 text-black-' />
+                </div>
             </div>
+            <figure className='px-6'>                    
+                <img 
+                    className=' w-full h-75% rounded-lg'
+                    src={context.productToShow.images} 
+                    alt={context.productToShow.title}
+                />
+                <p className='flex flex-col p-6'>
+                    <span className='font-medium text-2xl mb-2'>
+                        ${context.productToShow.price}
+                    </span>
+                    <span className='font-medium text-md'>
+                        {context.productToShow.title}
+                        </span>   
+                    <span className='font-light text-sm'>
+                        {context.productToShow.description}
+                    </span>   
+                </p>
+            </figure>
         </aside>
     )
 }
--- src/Context/index.jsx
+++ src/Context/index.jsx
@@ -4,13 +4,27 @@ import { useState } from 'react';
 export const ShoppingCartContext = createContext()
 
 export const  ShoppingCartProvider = ({children}) => {
+
+    //Contador del shopping cart
     const [count, setCount] = useState(0)
 
+    //abrir y cerrar aside con detalle del producto
+    const [isProductDetailOpen, setIsProductDetailOpen] = useState(false)
+    const openProductDetail = () => setIsProductDetailOpen(true)
+    const closeProductDetail = () => setIsProductDetailOpen(false)
+
+    //mostrar el producto detallado
+    const [productToShow, setProductToShow] = useState({})
 
     return (
         <ShoppingCartContext.Provider value={{
             count,
-            setCount
+            setCount,
+            openProductDetail,
+            closeProductDetail,
+            isProductDetailOpen,
+            setProductToShow,
+            productToShow
         }}>
             {children}
         </ShoppingCartContext.Provider>   
