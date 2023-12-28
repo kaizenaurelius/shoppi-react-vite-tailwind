0e91f347c225ad35de5dfbc457b4fdf29573f2ca
Tob Stoic <rafaeltobass@gmail.com>
2023-12-28 02:50:28 -0500

    ventana lateral de my order se abre y se cierra a de detalles

--- src/Context/index.jsx
+++ src/Context/index.jsx
@@ -1,39 +1,43 @@
-import { createContext } from 'react';
-import { useState } from 'react';
+import { createContext, useState } from 'react'
 
 export const ShoppingCartContext = createContext()
 
-export const  ShoppingCartProvider = ({children}) => {
+export const ShoppingCartProvider = ({children}) => {
+  // Shopping Cart · Increment quantity
+  const [count, setCount] = useState(0)
 
-    //Contador del shopping cart
-    //Agregar producto el carrito
-    const [count, setCount] = useState(0)
-    const [cartProducts, setCartProducts] = useState([]) 
+  // Product Detail · Open/Close
+  const [isProductDetailOpen, setIsProductDetailOpen] = useState(false)
+  const openProductDetail = () => setIsProductDetailOpen(true)
+  const closeProductDetail = () => setIsProductDetailOpen(false)
 
-    //abrir y cerrar aside con detalle del producto
-    const [isProductDetailOpen, setIsProductDetailOpen] = useState(false)
-    const openProductDetail = () => setIsProductDetailOpen(true)
-    const closeProductDetail = () => setIsProductDetailOpen(false)
+  // Checkout Side Menu · Open/Close
+  const [isCheckoutSideMenuOpen, setIsCheckoutSideMenuOpen] = useState(false)
+  const openCheckoutSideMenu = () => setIsCheckoutSideMenuOpen(true)
+  const closeCheckoutSideMenu = () => setIsCheckoutSideMenuOpen(false)
 
-    //mostrar el producto detallado
-    const [productToShow, setProductToShow] = useState({})
+  // Product Detail · Show product
+  const [productToShow, setProductToShow] = useState({})
 
-    
-    
+  // Shopping Cart · Add products to cart
+  const [cartProducts, setCartProducts] = useState([])
 
-    return (
-        <ShoppingCartContext.Provider value={{
-            count,
-            setCount,
-            openProductDetail,
-            closeProductDetail,
-            isProductDetailOpen,
-            setProductToShow,
-            productToShow,
-            cartProducts,
-            setCartProducts
-        }}>
-            {children}
-        </ShoppingCartContext.Provider>   
-    )
+  return (
+    <ShoppingCartContext.Provider value={{
+      count,
+      setCount,
+      openProductDetail,
+      closeProductDetail,
+      isProductDetailOpen,
+      productToShow,
+      setProductToShow,
+      cartProducts,
+      setCartProducts,
+      isCheckoutSideMenuOpen,
+      openCheckoutSideMenu,
+      closeCheckoutSideMenu
+    }}>
+      {children}
+    </ShoppingCartContext.Provider>
+  )
 }
