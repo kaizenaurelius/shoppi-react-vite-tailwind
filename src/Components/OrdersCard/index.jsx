bb9d78165b838e2b4c44b3599c597b864413e6ec
Tob Stoic <rafaeltobass@gmail.com>
2024-01-13 14:48:50 -0500

    Estilos a mis ordenes

--- src/Components/OrdersCard/index.jsx
+++ src/Components/OrdersCard/index.jsx
@@ -1,11 +1,11 @@
-import { ShoppingCartIcon, CurrencyDollarIcon, CalendarDaysIcon } from '@heroicons/react/24/solid'
+import { ShoppingCartIcon, CurrencyDollarIcon, CalendarDaysIcon, ChevronRightIcon } from '@heroicons/react/24/solid'
 
 const OrdersCard = props => {
   const { totalPrice, totalProducts, orderDate, } = props
 
   return (
-    <div className="flex justify-between items-center mb-3 border border-black">
-      <div className="flex items-center justify-between grow gap-2 px-4">
+    <div className="flex justify-between items-center mb-3 border border-black p-4 w-80 rounded-lg">
+      <div className="flex items-center justify-between grow gap-2 px-4 w-full">
           <div className="flex gap-1 items-center justify-center">
             <ShoppingCartIcon className="h-6 w-6 text-black" />
             <p className="font-light text-sm">{`${totalProducts} ${totalProducts === 1 ? "producto" : "productos"}`}</p>
@@ -17,6 +17,9 @@ const OrdersCard = props => {
           <div className="flex gap-1 items-center justify-center">
             <CalendarDaysIcon className="h-6 w-6 text-black" />
             <p className="font-light text-sm">{orderDate}</p>
+         </div>
+         <div className="flex gap-1 items-center justify-center">
+            <ChevronRightIcon className='h-6 w-6 text-black cursor-pointer'></ChevronRightIcon>
          </div>
       </div>
     </div>
