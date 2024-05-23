f70256b2bb4c374bfd24f10b0fb37d0571e213e2
Tob Stoic <rafaeltobass@gmail.com>
2024-05-23 14:28:38 -0500

    1-Renderizacion del navbar dependiendo del estado de signout 2- Maquetación de página sign-in

--- src/Components/Navbar/index.jsx
+++ src/Components/Navbar/index.jsx
@@ -7,6 +7,65 @@ const Navbar = () => {
   const context = useContext(ShoppingCartContext)
   const activeStyle = 'underline underline-offset-4'
 
+  //Sign-Out Verificación
+
+  const signOut = localStorage.getItem('sign-out')
+  const parsedSignOut = JSON.parse(signOut)
+  const isUserSignedOut = signOut || parsedSignOut
+
+  const renderConditionedNavElement = () => {
+    if(isUserSignedOut) {
+      <li>
+      <NavLink
+        to='/sign-in'
+        className={({ isActive }) =>
+          isActive ? activeStyle : undefined
+        }
+        onClick={() => handleSignOut()}>
+        Sign Out
+      </NavLink>
+    </li>
+    }else{
+      return(
+        <>
+        <li className='text-black/60'>
+          teff@platzi.com
+        </li>
+        <li>
+          <NavLink
+            to='/my-orders'
+            className={({ isActive }) =>
+              isActive ? activeStyle : undefined
+            }>
+            My Orders
+          </NavLink>
+        </li>
+        <li>
+          <NavLink
+            to='/my-account'
+            className={({ isActive }) =>
+              isActive ? activeStyle : undefined
+            }>
+            My Account
+          </NavLink>
+        </li>
+        <li>
+      <NavLink
+        to='/sign-in'
+        className={({ isActive }) =>
+          isActive ? activeStyle : undefined
+        }
+        onClick={() => handleSignOut()}>
+        Sign Out
+      </NavLink>
+    </li>
+  
+      
+      </>
+      )
+    }
+  }
+
   const handleSignOut = () => {
     localStorage.setItem('sign-out', JSON.stringify(true) ) //cambio a true en el LocalStorage y en el contexto.
     context.setSignOut(true)
@@ -82,44 +141,16 @@ const Navbar = () => {
         </li>
       </ul>
 
-
       <ul className='flex items-center gap-3'>
-        <li className='text-black/60'>
-          teff@platzi.com
-        </li>
-        <li>
-          <NavLink
-            to='/my-orders'
-            className={({ isActive }) =>
-              isActive ? activeStyle : undefined
-            }>
-            My Orders
-          </NavLink>
-        </li>
-        <li>
-          <NavLink
-            to='/my-account'
-            className={({ isActive }) =>
-              isActive ? activeStyle : undefined
-            }>
-            My Account
-          </NavLink>
-        </li>
-        <li>
-          <NavLink
-            to='/sing-in'
-            className={({ isActive }) =>
-              isActive ? activeStyle : undefined
-            }
-            onClick={() => handleSignOut()}>
-            Sign Out
-          </NavLink>
-        </li>
-        <li className='flex  items-center'>
+         {renderConditionedNavElement()}
+         <li className='flex  items-center'>
           <ShoppingBagIcon className='h-6 w-6 text-black-' /> 
           <div> { context.cartProducts.length} </div>
         </li>
       </ul>
+
+
+ 
     </nav>
   )
 }
